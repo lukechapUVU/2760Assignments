9218b8302cd1265676a0fbdbb42fda72c3cd9579
Luke Chapman <Luke.Chapman@ccr.net>
2021-04-17 20:40:41 -0600

    directory check test

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var loc = window.location;
+var loc = window.location.href;
 
-console.log(loc);
+console.log(loc + '/Unit01');
+
+var request;
+if(window.XMLHttpRequest)
+    request = new XMLHttpRequest();
+else
+    request = new ActiveXObject("Microsoft.XMLHTTP");
+request.open('GET', loc + '/Unit01', false);
+request.send(); 
+if (request.status === 404) {
+    alert("The page you are trying to reach is not available.");
+}
